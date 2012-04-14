12e0ebd6f70524ddb7087f975bebbee4e7b46cf9
Ryan Cromwell <ryan@echelontouch.com>
2012-04-14 19:08:01 -0400

    its about the ordinals... check it

--- specs/translate_spec.js
+++ specs/translate_spec.js
@@ -1,4 +1,12 @@
-var translate = require('../translate');
+var translate = require('../translate'),
+    _ = require('../underscore');
+
+var log = function(msg) {
+  console.log(msg);
+};
+
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var place = function() { return alphabet.indexOf('a'); };
 
 describe('translate', function() {
   it('a -> y', function() {
@@ -12,4 +20,18 @@ describe('translate', function() {
   it('z -> q', function() {
     expect( translate('z') ).toBe('q');
   });
+
+  it('index of a is 0', function() {
+    expect( place('a') ).toBe(0);
+  });
+
+  it('y - a === e - o places', function() {
+    var left = place('y') - place('a');
+    var right = place('e') - place('o');
+
+    expect(left).toBe(right);
+  });
+  
 });
+
+return;
